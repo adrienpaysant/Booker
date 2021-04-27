f3a6d88aa1b142894c22d74612a427828787be59
Monnet Joris <joris.monnet@he-arc.ch>
2021-04-27 10:40:29 +0200

    protect urls

--- Booker/Models/Book.cs
+++ Booker/Models/Book.cs
@@ -9,17 +9,28 @@ namespace Booker.Models
     public class Book
     {
         [Key]
+        [Required]
         public int ISBN { get; set; }
+        [Required]
         public string Title { get; set; }
-        public string BookerUserId{ get; set; }
-        public string Author{ get; set; }
+        [Required]
+        public string BookerUserId { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
         public string Editor { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        [Required]
         public string Image { get; set; }
-        public string Categories{ get; set; }
-        public string BuyLink{ get; set; }
+        [Required]
+        [Display(Name = "Categories (separated by a comma)")]
+        public string Categories { get; set; }
+        [Required]
+        public string BuyLink { get; set; }
 
     }
 }
